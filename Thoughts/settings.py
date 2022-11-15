22500998e8800332201defc9ac6e8d3d8e9b9db7
Breno Brito <breno.silvabrito@hotmail.com>
2022-11-15 10:29:23 +0000

    Connect to mysql

--- Thoughts/settings.py
+++ Thoughts/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'Thoughts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'thoughts',
+        'USER': 'root',
+        'PASSWORD': 'root123',
+        'HOST': 'localhost',
     }
 }
 

2b6b7e5626b1fc56d2e5cc89878641041f4d7e4a
Breno Brito <breno.silvabrito@hotmail.com>
2022-11-21 21:01:59 +0000

    Create Signup page

--- Thoughts/urls.py
+++ Thoughts/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('ThoughtsApp.urls'), name='home'),
     path('thoughts/', include('ThoughtsApp.urls'), name='home'),
     path('admin/', admin.site.urls),
+    path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path('', TemplateView.as_view(template_name='ThoughtsApp/index.html'), name='home'),
     path('accounts/login/', TemplateView.as_view(template_name='login.html'), name='login')
